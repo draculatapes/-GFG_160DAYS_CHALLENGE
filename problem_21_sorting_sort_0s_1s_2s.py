de63d23530e19dcd48712d93ed8b4765779935a0
Amarjeet Kumar <91472661+draculatapes@users.noreply.github.com>
2024-12-05 23:23:06 +0530

    Update problem_21_sorting_sort_0s_1s_2s.py

--- problem_21_sorting_sort_0s_1s_2s.py
+++ problem_21_sorting_sort_0s_1s_2s.py
@@ -1,3 +1,4 @@
+#Solution1: Two Pass Solution
 class Solution:
     # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
@@ -16,3 +17,22 @@ class Solution:
                 arr[j]=arr[i]
                 arr[i]=temp
         return arr
+#Solution:2 Single Pass Solution( Dutch National Flag Algorithm)
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        low, mid, high = 0, 0, len(arr) - 1
+        
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:  # arr[mid] == 2
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+        
+        return arr
+
