6e311d6b97c35d3a0fdf114e76101bc589eb1811
Amarjeet Kumar <91472661+draculatapes@users.noreply.github.com>
2024-12-09 22:13:28 +0530

    Update problem_24_sorting_overlapping_intervals.py

--- problem_24_sorting_overlapping_intervals.py
+++ problem_24_sorting_overlapping_intervals.py
@@ -1,24 +1,12 @@
 class Solution:
-    def insertInterval(self, intervals, newInterval):
-        # Code here
-        i=0
-        res=[]
-        while (i<len(intervals) and intervals[i][0]<=newInterval[0]):
-            res.append(intervals[i])
-            i+=1
-        if len(res)==0:
-            res.append(newInterval)
-        else:
-            if res[-1][1]>=newInterval[0]:
-                res[-1][0]=min(res[-1][0],newInterval[0])
-                res[-1][1]=max(res[-1][1],newInterval[1])
-            else:
-                res.append(newInterval)
-        while i<len(intervals):
-            if res[-1][1]>=intervals[i][0]:
-                res[-1][0]=min(res[-1][0],intervals[i][0])
-                res[-1][1]=max(res[-1][1],intervals[i][1])
-            else:
-                res.append(intervals[i])
-            i+=1
-        return res
+	def mergeOverlap(self, arr):
+		#Code here
+		arr.sort()
+		res=[arr[0]]
+		for i in range(1,len(arr)):
+		    if arr[i][0]<=res[-1][1]:
+		        res[-1][0]=min(res[-1][0],arr[i][0])
+		        res[-1][1]=max(res[-1][1],arr[i][1])
+		    else:
+		        res.append(arr[i])
+		return res
