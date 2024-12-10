3cca249be859b8a23492e7e2f8922589ae58095c
Amarjeet Kumar <91472661+draculatapes@users.noreply.github.com>
2024-12-10 21:42:45 +0530

    Update problem_26_sorting_non_overlapping_intervals.py

--- problem_26_sorting_non_overlapping_intervals.py
+++ problem_26_sorting_non_overlapping_intervals.py
@@ -1,12 +1,16 @@
 class Solution:
-	def mergeOverlap(self, arr):
-		#Code here
-		arr.sort()
-		res=[arr[0]]
-		for i in range(1,len(arr)):
-		    if arr[i][0]<=res[-1][1]:
-		        res[-1][0]=min(res[-1][0],arr[i][0])
-		        res[-1][1]=max(res[-1][1],arr[i][1])
-		    else:
-		        res.append(arr[i])
-		return res
+    def minRemoval(self, intervals):
+        # Code here
+        intervals.sort()
+        start,end=intervals[0]
+        i=1
+        res=0
+        while i<len(intervals):
+            if end>intervals[i][0]:
+                res+=1
+                if end>intervals[i][1]:
+                    start,end=intervals[i]
+            else:
+                start,end=intervals[i]
+            i+=1
+        return res
