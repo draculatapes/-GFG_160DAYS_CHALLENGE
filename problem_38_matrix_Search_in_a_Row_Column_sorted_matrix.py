18f326d1151c4e78a8d0838078aa66becad9666a
Amarjeet Kumar <91472661+draculatapes@users.noreply.github.com>
2024-12-23 22:56:08 +0530

    Update problem_38_matrix_Search_in_a_Row_Column_sorted_matrix.py

--- problem_38_matrix_Search_in_a_Row_Column_sorted_matrix.py
+++ problem_38_matrix_Search_in_a_Row_Column_sorted_matrix.py
@@ -1,3 +1,4 @@
+#Approach 1: T(n)=O(m*logn)
 class Solution:
 	def matSearch(self, mat, x):
 		# Complete this function
@@ -12,4 +13,19 @@ class Solution:
 		            high=mid-1
 		        else:
 		            low=mid+1
+	    return False
+#Approach 2: T(n)=O(m+n) 
+class Solution:
+	def matSearch(self, mat, x):
+		# Complete this function
+		m,n=len(mat),len(mat[0])
+		i,j=0,n-1
+		while i<m and j>=0:
+		    if mat[i][j]==x:
+		        return True
+		    if mat[i][j]>x:
+		        j-=1
+		    else:
+		        i+=1
+		        
 	    return False
